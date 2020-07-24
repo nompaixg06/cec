f89c4e29346516165a24e347501ea86b14aa2c01
gnompaixg06 <emailisparks@gmail.com>
2020-07-24 22:52:11 +0200

    active bug

--- menu.php
+++ menu.php
@@ -17,3 +17,19 @@
 		</div>
 	</div>
 	</header>
+	<script>
+	// Get the container element
+var btnContainer = document.getElementById("nav_buttons");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("button_link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
+</script>
