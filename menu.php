64da765dd4443491cb84a11b2f3ebfc44cb962cd
gnompaixg06 <emailisparks@gmail.com>
2020-07-24 21:34:48 +0200

    active bug

--- menu.php
+++ menu.php
@@ -13,6 +13,18 @@
 				<li class="nav_items nav_logo"><img class="logo" src="https://i.imgur.com/3MTQzk0.png"></li>
 				<li class="nav_items nav_buttons"><a class="button_link" href="#">DEVIS</a></li>
 				<li class="nav_items nav_buttons"><a class="button_link" href="#">CONTACT</a></li>
+				<?php
+        // Define each name associated with an URL
+        $urls = array(
+            'ACCUEIL' => '/',
+            'GAMME' => '/gamme.php',
+        );
+
+        foreach ($urls as $name => $url) {
+            print '<li '.(($currentPage === $name) ? ' class="active" ': '').
+                '><a href="'.$url.'">'.$name.'</a></li>';
+        }
+    ?>
 			</ul>
 		</div>
 	</div>
