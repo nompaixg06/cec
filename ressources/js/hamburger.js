ed99fce38d0f17cc864ac1df04cbf5a5a794ebbc
nompaixg06 <61711643+nompaixg06@users.noreply.github.com>
2020-07-24 12:55:04 +0200

    Add files via upload

---
+++ ressources/js/hamburger.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav_container');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menu.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menu.classList.remove('open');
+    menuOpen = false;
+  }
+});
