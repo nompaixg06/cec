ead94b815720e4f14aad25c2656ad1831b1f2183
gnompaixg06 <emailisparks@gmail.com>
2020-07-24 22:04:03 +0200

    active bug

--- menu.php
+++ menu.php
@@ -12,11 +12,11 @@ echo $page;
 		<div class="nav_and_logo_container">
 			<div class="hidden_logo_container"><img class="hidden_logo" src="https://i.imgur.com/3MTQzk0.png"></div>
 			<ul class="nav_container" id="buttons_container">
-				<li class="nav_items nav_buttons active" <?php if($page=="/index.php") echo'class="active"'; ?>><a class="button_link" href="#">ACCUEIL</a></li>
-				<li class="nav_items nav_buttons" <?php if($page=="/gamme.php") echo'class="active"'; ?>><a class="button_link" href="#">GAMME</a></li>
+				<li class="nav_items nav_buttons"><a class="button_link" href="#" <?php if($page=="/index.php") echo'class="active"'; ?>>ACCUEIL</a></li>
+				<li class="nav_items nav_buttons"><a class="button_link" href="#" <?php if($page=="/gamme.php") echo'class="active"'; ?>>GAMME</a></li>
 				<li class="nav_items nav_logo"><img class="logo" src="https://i.imgur.com/3MTQzk0.png"></li>
-				<li class="nav_items nav_buttons" <?php if($page=="/devis.php") echo'class="active"'; ?>><a class="button_link active" href="#">DEVIS</a></li>
-				<li class="nav_items nav_buttons" <?php if($page=="/index.php") echo'class="active"'; ?>><a class="button_link" href="#">CONTACT</a></li>
+				<li class="nav_items nav_buttons"><a class="button_link" href="#" <?php if($page=="/devis.php") echo'class="active"'; ?>>DEVIS</a></li>
+				<li class="nav_items nav_buttons"><a class="button_link" href="#" <?php if($page=="/index.php") echo'class="active"'; ?>>CONTACT</a></li>
 			</ul>
 		</div>
 	</div>
