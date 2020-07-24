19e02e937bf7940332befaccb97273390a791f58
gnompaixg06 <emailisparks@gmail.com>
2020-07-24 21:21:18 +0200

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> C.E.C </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="ressources/css/menu.css" type="text/css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet"> 
+</head>
+<body>
+	<!-- HEADER-->
+	<?php include('menu.php'); ?>
+	<!-- PAGE-->
+</body>
+<script src="ressources/js/hamburger.js"></script>
+</html>
--- menu.php
+++ menu.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title> C.E.C </title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="ressources/css/index.css" type="text/css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet"> 
-</head>
-<body>
-	<!-- HEADER-->
-	<header>
+<header>
 		<div class="header_container">
 		<div class="menu_button_container">
 			<div class="menu-btn">
@@ -28,7 +17,3 @@
 		</div>
 	</div>
 	</header>
-	<!-- PAGE-->
-</body>
-<script src="ressources/js/hamburger.js"></script>
-</html>
