21ae255a4cf9fc56522dde8f826f5b68e4e11a85
gnompaixg06 <emailisparks@gmail.com>
2020-07-24 21:57:23 +0200

    active bug

--- menu.php
+++ menu.php
@@ -12,7 +12,7 @@ echo $page;
 		<div class="nav_and_logo_container">
 			<div class="hidden_logo_container"><img class="hidden_logo" src="https://i.imgur.com/3MTQzk0.png"></div>
 			<ul class="nav_container" id="buttons_container">
-				<li class="nav_items nav_buttons" <?php if($page=="/index.php") echo'class="active"'; ?>><a class="button_link" href="#">ACCUEIL</a></li>
+				<li class="nav_items nav_buttons active" <?php if($page=="/index.php") echo'class="active"'; ?>><a class="button_link" href="#">ACCUEIL</a></li>
 				<li class="nav_items nav_buttons" <?php if($page=="/gamme.php") echo'class="active"'; ?>><a class="button_link" href="#">GAMME</a></li>
 				<li class="nav_items nav_logo"><img class="logo" src="https://i.imgur.com/3MTQzk0.png"></li>
 				<li class="nav_items nav_buttons" <?php if($page=="/devis.php") echo'class="active"'; ?>><a class="button_link" href="#">DEVIS</a></li>
